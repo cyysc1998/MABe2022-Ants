209cc647f945eec5cfa67a0c8cf38388ba0b558a
chao-sun <18245101590@163.com>
2022-05-13 05:28:31 +0000

    a01_more_frame

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 export PYTHONPATH=$PYTHONPATH:.
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/00_baseline.yml
+CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/01_more_frame_70k_iter.yml &&
+CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/01_more_frame_90k_iter.yml
