611786000f36732f9fc98d692e483cd81fb46496
chao-sun <18245101590@163.com>
2022-05-17 05:09:38 +0000

    Add interence config

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 export PYTHONPATH=$PYTHONPATH:.
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/02_crop_aug_60k_iter.yml &&
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/02_crop_aug_80k_iter.yml
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/03_re_60k_iter.yml &&
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/03_re_100k_iter.yml &&
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/04_guassian_blur_20k_iter.yml &&
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/04_guassian_blur_90k_iter.yml
