97a244c793fd4c55acbce246aab39f14d2afde96
chao-sun <18245101590@163.com>
2022-05-17 13:33:00 +0000

    a05 val

--- linear_eval.sh
+++ linear_eval.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:.
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4321 mabe/linear_eval.py -opt options/03_re.yml
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4321 mabe/linear_eval.py -opt options/05_td.yml
--- val.sh
+++ val.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:.
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4321 mabe/val.py -opt options/03_re.yml
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4321 mabe/val.py -opt options/05_td.yml
