08eec267724ab12de900a09c9a87d490b9a43d9b
chao-sun <18245101590@163.com>
2022-05-24 08:24:22 +0000

    add inference code

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-export PYTHONPATH=$PYTHONPATH:.
-# CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/05_td_10k_iter.yml &&
-# CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/05_td_30k_iter.yml &&
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/07_m_views_60k_iter.yml &&
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/07_m_views_90k_iter.yml
+bash test.sh
+cd ../dev && bash train.sh
--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:.
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/03_re_60k_iter.yml &&
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/03_re_100k_iter.yml &&
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/04_guassian_blur_20k_iter.yml &&
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/04_guassian_blur_90k_iter.yml
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/10_s3d_20k_iter.yml &&
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/10_s3d_40k_iter.yml &&
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/inference/10_s3d_70k_iter.yml
