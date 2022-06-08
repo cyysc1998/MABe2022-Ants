7688b97383912ed250f5687670c09253400bb89a
chao-sun <18245101590@163.com>
2022-06-08 07:33:55 +0000

    update eval code

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:.
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/17_moco_base.yml
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/21_sample_t.yml
