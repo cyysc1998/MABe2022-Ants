b502c772d0c43487f49e61d4b993b81fecde28e6
chao-sun <18245101590@163.com>
2022-06-27 05:53:32 +0000

    a32_more_epoch

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:.
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/30_r101.yml
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=4322 mabe/test.py -opt options/32_r101_100k_iter.yml
